525b11afeff13ffc7bd65155292710602c5e916c
Thumay Huynh <tvh4xuk@virginia.edu>
2024-04-10 19:24:32 -0400

    added level 1, somewhat working scene switches

---
+++ Assets/Scripts/CameraFollow2.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow2 : MonoBehaviour
+{
+    public Transform player;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.x = player.transform.position.x;
+        newPosition.y = player.transform.position.y;
+        transform.position = newPosition;
+    }
+}
