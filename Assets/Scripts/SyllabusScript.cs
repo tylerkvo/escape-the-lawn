e4f0075cefab81bb8e8f0dc618599b0eb8c1b236
Tyler Vo <tyler.k.vo@gmail.com>
2024-04-24 21:10:25 -0400

    Added syllabus

---
+++ Assets/Scripts/SyllabusScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyllabusScript : MonoBehaviour
+{
+    public GameObject SyllabusPanel; 
+
+    void Start()
+    {
+        SyllabusPanel.SetActive(true);
+        Time.timeScale = 0f; // Pause the game time
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            SyllabusPanel.SetActive(false); // Hide the instructions panel
+            Time.timeScale = 1f; // Resume the game time
+        }
+    }
+}
